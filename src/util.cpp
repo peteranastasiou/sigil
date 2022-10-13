cfc00192acf97adee82c2bfdb7b4825b05954c44
Peter Anastasiou <8896307+peteranastasiou@users.noreply.github.com>
2022-10-14 08:54:57 +1030

    fix format function

--- src/util.cpp
+++ src/util.cpp
@@ -5,27 +5,30 @@
 
 namespace util {
 
-void format(const char* fmt, ...) {
-    // guess at buffer size:
-    size_t initLen = 32;
-    char * buf = new char[initLen];
-
+std::string format(const char* fmt, ...) {
     va_list args;
+
+    // guess at buffer size:
+    size_t initLen = 8;
+    char * buf = (char *) malloc(initLen);  // use malloc so we can reallocate
+
+    // Have a crack with the buffer size & find out actual size
     va_start(args, fmt);
-    
-    // Get actual length needed:
     size_t len = vsnprintf(buf, initLen, fmt, args);
-    
-    // Check if we got everything
-    if( len >= initLen ){
-        delete buf;
-        buf = new char[len+1];
-        vsnprintf(buf, len+1, fmt, args);
-    }
     va_end(args);
 
+    // Check if we got everything
+    if( len >= initLen ){
+        // Not big enough - resize: 
+        buf = (char *) realloc(buf, len+1);
+
+        // format to string again:
+        va_start(args, fmt);
+        vsnprintf(buf, len+1, fmt, args);
+        va_end(args);
+    }
     std::string s = std::string(buf);
-    delete buf
+    free(buf);
     return s;
 }
 
